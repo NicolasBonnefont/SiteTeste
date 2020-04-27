5523f2ffcdbae510e249cde7d5f94caa9c347071
Nicolas <nicolas.7l@hotmail.com>
2020-04-27 16:20:19 -0300

    acertos

--- start/routes.js
+++ start/routes.js
@@ -13,6 +13,7 @@ Route.on('/PedidoDeVendas').render('/PedidoDeVendas')
 Route.on('/NFe').render('/NFe')
 Route.on('/RelatorioFaturamento').render('/RelatorioFaturamento')
 Route.on('/contasapagar').render('/contasapagar')
+Route.on('/contasareceber').render('/contasareceber')
 
 
 
